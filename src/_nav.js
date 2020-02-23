0e8c16201ef342ce75f16c775e6434074e646c2b
gnelabs1@gmail.com <ec2-user@ip-172-31-43-254.us-east-2.compute.internal>
2020-02-23 18:36:11 +0000

    Initial bones for stack manager website.

---
+++ src/_nav.js
@@ -0,0 +1,32 @@
+export default {
+  items: [
+    {
+      title: true,
+      name: 'Tools',
+      wrapper: {
+        element: '',
+        attributes: {}
+      },
+      class: ''
+    },
+    {
+      name: 'Pipeline',
+      url: '/',
+      icon: 'fa fa-ellipsis-h',
+    },
+    {
+      title: true,
+      name: 'Info',
+      wrapper: {
+        element: '',
+        attributes: {}
+      },
+      class: ''
+    },
+    {
+      name: 'Documentation',
+      url: 'https://github.com/gnelabs/EpithyStackManager',
+      icon: 'fa fa-file-text-o',
+    }
+  ]
+};
